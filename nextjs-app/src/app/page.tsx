2b7c3898b02e2ce30b90cf09e80f9216c78d571f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-07-10 17:22:45 +0400

    style: respond with message on user input

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import FibWasm from "@/components/FibWasm";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome to</h1>
       <FibWasm />
     </div>
   );
--- nextjs-app/src/components/FibWasm.tsx
+++ nextjs-app/src/components/FibWasm.tsx
@@ -5,22 +5,54 @@ import * as wasm from "fib_wasm";
 
 export default function FibWasm() {
   const [value, setValue] = useState<number | null>(null);
-  const variable = 15;
+  const [variable, setVariable] = useState<number>(0);
+  const [t0, setT0] = useState<number>(0);
+  const [t1, setT1] = useState<number>(0);
+  const minVariable = 0;
+  const maxVariable = 40;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = Number(e.target.value);
+    if (!isNaN(val) && val >= minVariable && val <= maxVariable) {
+      setVariable(val);
+    } else {
+      alert(`please enter values between ${minVariable}-${maxVariable}`);
+    }
+  };
+
+  // useEffect(() => {
+  //   const result = wasm.fib(variable);
+  //   setValue(result);
+  // }, [variable]);
 
   useEffect(() => {
-    (async () => {
-      const result = wasm.fib(variable);
-      setValue(result);
-    })();
-  }, []);
+    setT0(performance.now());
+    const result = wasm.fib(variable);
+    setT1(performance.now());
+    setValue(result);
+  }, [variable]);
 
   return (
     <div>
-      <h2>Rust + Wasm</h2>
+      <h2>Rust + wasm + Nextjs app</h2>
+      <div>
+        This is a fibonacci calcularor using Rust compiled in web assembly
+      </div>
+      <div>
+        Please, enter a value between {minVariable}-{maxVariable}:{" "}
+      </div>
+      <input
+        type="number"
+        className="border"
+        value={variable}
+        onChange={handleChange}
+      />
       {value !== null ? (
-        <p>
-          fib({variable}) = {value}
-        </p>
+        <>
+          <p>
+            fib({variable}) = {value}
+          </p>
+          <p>computed in {t1 - t0} ms</p>
+        </>
       ) : (
         <p>Loading...</p>
       )}
